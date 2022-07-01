013647b0ff03f28cae003edd01f5d1b54de4c11a
Vladimirova Ekaterina <echapyrkina@mail.ru>
2022-07-01 16:09:13 +0300

    lesson 18

--- src/index.js
+++ src/index.js
@@ -1,17 +1,63 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import configureStore from './store/store';
+import { titleChanged, taskDeleted, loadTasks, getTasks, completeTask, getTasksLoadingStatus, createTask } from './store/task';
+import { useSelector, useDispatch } from "react-redux";
+import { getError } from "./store/errors";
 
+
+const store = configureStore();
+const App = (params) => {
+  const state = useSelector(getTasks());
+  const isLoading = useSelector(getTasksLoadingStatus());
+  const error = useSelector(getError());
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(loadTasks())
+  }, []);
+
+  const changeTitle = (taskId) => {
+    dispatch(titleChanged(taskId))
+  }
+  const deleteTask = (taskId) => {
+    dispatch(taskDeleted(taskId))
+  }
+  const taskCreate = () => {
+    dispatch(createTask({}))
+  }
+  if (isLoading) {
+        return <h1>Loading</h1>;
+  }
+  if (error) {
+        return <p>{error}</p>;
+  }
+  return (
+  <>
+    <h1>App</h1>
+    <button onClick = {() => taskCreate()}>Create task</button>
+    <hr/>
+    <ul>
+      {state.map((el) => (
+        <li key={el.id}>
+        <p>{el.title}</p>
+        <p>{`Completed: ${el.completed}`}</p>
+        <button onClick = {() => dispatch(completeTask(el.id))}>Complete</button>
+        <button onClick = {() => changeTitle(el.id)}>Change title</button>
+        <button onClick = {() => deleteTask(el.id)}>Delete task</button>
+        <hr/>
+        </li>
+        ))}
+    </ul>
+  </>
+  )
+  
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+  <Provider store={store}>
     <App />
+  </Provider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/store/store.js
@@ -0,0 +1,20 @@
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import errorReducer from "./errors";
+import taskReducer from "./task";
+import { logger } from "./middleware/logger";
+
+const rootReducer = combineReducers({
+    errors: errorReducer,
+    tasks: taskReducer,
+});
+
+function createStore() {
+    return configureStore({
+        reducer: rootReducer,
+        middleware: (getDefaultMiddleware) =>
+            getDefaultMiddleware().concat(logger),
+        devTools: process.env.NODE_ENV !== "production",
+    });
+}
+
+export default createStore;
